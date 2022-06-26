6f41c92171f11d00590e6dce5cf07c02adb0c4c4
MaximSleegers2001 <BurnGFXEmail@gmail.com>
2022-06-26 22:49:19 +0200

    Update doe-mee.svelte

--- src/lib/doe-mee.svelte
+++ src/lib/doe-mee.svelte
@@ -13,31 +13,24 @@
 </section>
 <section>
     <div class="cont">
-         <div class="img">
-            <!-- <img alt="img" src=> -->
-         </div>
-            <div class="text">
                     <h1>Doe Mee!</h1>
                     Naast het ontvangen van de nieuwsbrief en daarmee vriend worden van Nieuw Brabant Zuid-Oost kun je bij ons aan te sluiten of nader kennis te maken: 
 
-<br> Ontmoet elkaar op de maandelijkse borrel <br>
+<br> <br> Ontmoet elkaar op de maandelijkse borrel <br> <br>
 
 We organiseren eens per maand een borrel. In een informele setting komen we bij elkaar onder het genot van een drankje en een hapje maken we graag kennis met jou en delen we de laatste ontwikkelingen.
 
-<br>Word mede-eigenaar<br>
+<br><br> Word mede-eigenaar<br> <br>
 
 Als mede-eigenaar heb je een aantal voordelen. Zo kun je voor een lager tarief deelnemen aan activiteiten, krijg je toegang tot de kennisbank en kun je meebeslissen over de thema's en investeringen van de coöperatie. Het mede-eigenaarschap van de regio Nieuw Brabant Zuid-Oost kost 35 euro per jaar. 
 
-<br>Support Society 4.0<br>
+<br><br> Support Society 4.0<br> <br>
 
 Ook kun je ook nog de landelijke Society 4.0 organisatie steunen; dat kost dat 15 euro per jaar extra en hiermee maak je het mogelijk dat de landelijke organisatie verder kan groeien.
 
-<br>Word kerngroeplid<br>
+<br><br> Word kerngroeplid<br> <br>
 
 Als kerngroeplid doe je mee aan één van onze projecten. Bij het kopje "Wij zoeken" kun je vinden waar wij nu specifiek naar op zoek zijn. Je draagt bij vanuit je eigen missie. Er er wordt geen vergoeding gegeven. Na aanmelding nemen we contact met je op.
-
-
-            </div>
     </div>
  </section>
 
@@ -130,7 +123,8 @@ img{
 }
 
 .text{
-   text-align: left;
+    content-align: center;
+   text-align: center;
    font-size: 20px;
 }
 
